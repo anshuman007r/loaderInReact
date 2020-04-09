1f998615e5d333aefb391d3d95b936ba4aa6ecc3
Anshuman Anand <anshu007r@gmail.com>
2020-04-09 17:51:20 +0530

    final update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
 import React, { Component } from 'react'
 import'./App.css';
+import {data} from './utils';
 export default class App extends Component {
   constructor(props)
   {
     super(props);
     this.state = {
       data: [],
-      mobile:[],
       loader: true
     };
   }
   componentDidMount()
   {
     setTimeout(() => {this.setState({
-      data: ['Anshuman', 'Arpit', 'Shashwat'],
-      mobile:['9912123456','9883125678','8787878787'],
+      data,
       loader: false
     })},2000);
 
   }
-  // displayDetail=(item,index1)=>{
-  //    return(
-  //      <p>{item}</p>
-  //    )
-  // }
+  getValue=()=>{
+    return this.state.data.map((item) => {
+      return(
+        <>
+          <p>Name: {item.name}</p>
+          <p>Mobile: {item.mobile}</p>
+        </>
+      )
+    });
+  }
   render() {
   
     return (
       <div id="show" className="renderContent">
          {this.state.loader && (<p>Loading ...</p>)}
-         {this.state.data.map((items)=>(<p>Name:{items}</p>))}
+         <p>{this.getValue()}</p>
       </div>
     )
   }
---
+++ src/utils.js
@@ -0,0 +1,14 @@
+export const data = [
+    {
+      name:'Anshuman',
+      mobile:'9912123456',
+    },
+    {
+      name:'Arpit',
+      mobile:'9883125678',
+    },
+    {
+      name: 'Shashwat',
+      mobile:'8787878787',
+    },
+  ];
